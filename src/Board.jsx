da00f66b11ca1b0a9eacd4e6c7887b551b55a923
Kamil Marczak <kamil.codemate@gmail.com>
2022-12-19 15:05:49 +0100

    Add body position memorization

--- src/Board.jsx
+++ src/Board.jsx
@@ -5,41 +5,52 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      head: [1, 0],
+      head: [0, 2],
       direction: null,
+      body: [[0, 0]],
+      snakeLength: 1,
     };
     this.move = this.move.bind(this);
   }
   componentDidMount() {
     document.addEventListener('keydown', (e) => {
-      debugger;
       if (e.key === 'ArrowLeft' || e.key === 'a' || e.key === 'A') this.setState({ direction: 'left' });
       if (e.key === 'ArrowRight' || e.key === 'd' || e.key === 'D') this.setState({ direction: 'right' });
       if (e.key === 'ArrowUp' || e.key === 'w' || e.key === 'W') this.setState({ direction: 'top' });
       if (e.key === 'ArrowDown' || e.key === 's' || e.key === 'S') this.setState({ direction: 'bottom' });
     });
-    setInterval(() => this.move(), 500);
+    setInterval(() => this.move(), 100);
   }
 
   move() {
+    let newBody = this.state.body;
+    newBody.unshift(this.state.head);
+    if (newBody.length > this.state.snakeLength) {
+      newBody.pop();
+    }
+
     switch (this.state.direction) {
       case 'left':
         this.setState((prevState) => ({
           head: [prevState.head[0], prevState.head[1] - 1],
+          body: newBody,
         }));
         break;
       case 'right':
         this.setState((prevState) => ({
           head: [prevState.head[0], prevState.head[1] + 1],
+          body: newBody,
         }));
         break;
       case 'top':
         this.setState((prevState) => ({
+          body: newBody,
           head: [prevState.head[0] - 1, prevState.head[1]],
         }));
         break;
       case 'bottom':
         this.setState((prevState) => ({
+          body: newBody,
           head: [prevState.head[0] + 1, prevState.head[1]],
         }));
         break;
@@ -47,7 +58,6 @@ class Board extends React.Component {
         break;
     }
   }
-
   render() {
     let retSquares = [];
     for (let i = 0; i < 15; i++) {
@@ -60,4 +70,5 @@ class Board extends React.Component {
     return <div className='Board'>{retSquares}</div>;
   }
 }
+
 export default Board;
